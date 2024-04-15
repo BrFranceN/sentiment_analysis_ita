60c728687149b07bf35519ea45223b20e46e6906
BrFranceN <workbrfrancen@gmail.com>
2024-04-15 12:07:51 +0200

    load data

--- script.py
+++ script.py
@@ -5,13 +5,19 @@ import json
 data = []
 with open("haspeede3-task1-test-data.jsonl", 'r', encoding='utf-8') as f:
     for line in f:
-        data.append(json.loads(line))
-
+        sample = json.loads(line)
+        text = sample['text']
+        choices = sample['choices']
+        index_label = int(sample['label'])
+        label = choices[index_label]
+        label = 0 if (label=='neutrale') else 1
+        data.append((text,label))
 # Now the 'data' list contains the data from the .jsonl file
 
 
-print(data[0])
-print(data[1])
-print(data[2])
-print(data[3])
-print(data[4])
+for elem in data[:10]:
+    print('======')
+    print(elem)
+    print('======')
+
+
